901ba012ade4002be850a9aa0ada21f6928fadb2
Elisaveta Todorova <eli961@abv.bg>
2016-10-10 00:01:29 +0300

    Adding other startup files.

---
+++ Java Basic OOP/Java OOP Basics Exam - 10 July 2016/src/system/split/Main.java
@@ -0,0 +1,17 @@
+package system.split;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by ELISAV on 9.10.2016 г..
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+
+    }
+
+}
